28838fd35ac7da50f53e9a71b8dfd42769709861
Pierre-Louis <pl.duthoit@gmail.com>
2018-06-27 11:13:49 +0100

    Add Dropdown

--- src/components/filter.component.js
+++ src/components/filter.component.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import Select from 'react-select';
+import 'react-select/dist/react-select.css';
 import logo from './../logo.svg';
 import './../App.css';
 
 const filter = {
-    backgroundColor: 'rgba(255,255,255,0.7)',
+    backgroundColor: 'rgba(255,255,255,1)',
     position: 'absolute',
     borderRadius: 3,
     boxShadow: '0 3px 4px rgba(0,0,0,0.2)',
@@ -18,15 +20,56 @@ const filter = {
     display: 'flex',
     flexDirection: 'column',
     flex: 1,
-    fontFamily: 'Galada'
+    fontFamily: 'El Messiri'
+};
+
+const filterCtn = {
+    flexDirection: 'row',
+    flexWrap: 'nowrap',
+    fontFamily: 'Roboto'
+};
+
+const selectStyle = {
+    maxWidth: '170px',
+    backgroundColor: 'rgba(240,240,240,0.4)',
 };
 
 class Filter extends Component {
+    state = {
+        selectedOption: '',
+    }
+    
+    handleChange = (selectedOption) => {
+        this.setState({ selectedOption });
+        // selectedOption can be null when the `x` (close) button is clicked
+        if (selectedOption) {
+            console.log(`Selected: ${selectedOption.label}`);
+        }
+    }
+
     render() {
+        const { selectedOption } = this.state;
+
         return (
             <div style={filter}>
                 <img src={logo} className="App-logo" alt="logo" />
                 <h1 className="App-title">Analyse des sports</h1>
+
+                {/* <input type="checkbox" id="test1"/> */}
+
+                <div style={filterCtn}>
+                    <label for="test1">Département</label>
+                    <Select
+                        name="form-field-name"
+                        value={selectedOption}
+                        onChange={this.handleChange}
+                        options={[
+                        { value: 'one', label: 'One' },
+                        { value: 'two', label: 'Two' },
+                        ]}
+                        style={selectStyle}
+                    />
+                </div>
             </div>
         );
     }
