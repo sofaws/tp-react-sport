8936072303e368b9ad235b48ad05154943c86b6c
Pierre-Louis <pl.duthoit@gmail.com>
2018-06-27 10:20:02 +0100

    Change style filter

--- src/components/filter.component.js
+++ src/components/filter.component.js
@@ -3,12 +3,20 @@ import logo from './../logo.svg';
 import './../App.css';
 
 const filter = {
-    flex: "25%",
-    backgroundColor: 'blue',
+    backgroundColor: 'white',
     position: 'absolute',
-    top: 100,
-    left: 50,
-    zIndex: 100
+    borderRadius: 3,
+    boxShadow: '0 3px 4px rgba(0,0,0,0.2)',
+    left: 30,
+    zIndex: 100,
+    height: 'calc(100% - 130px)',
+    marginTop: 60,
+    minWidth: '210px',
+    marginBottom: 30,
+    padding: 20,
+    display: 'flex',
+    flexDirection: 'column',
+    flex: 1
 };
 
 class Filter extends Component {
